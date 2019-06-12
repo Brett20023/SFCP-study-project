bea2d95894e0047c77eff1f1a09ad01e82e38ac2
LydiaJianLin <45258042+LydiaJianLin@users.noreply.github.com>
2019-06-12 22:20:15 +0800

    Add files via upload

---
+++ booking/booking.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="icon" href="images/favicon.png">
+	<title>Prince AL's Royal</title>
+
+	<link rel="stylesheet" href="css/materialize.css">
+    <link rel="stylesheet" href="css/fontawesome.min.css">
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body >
+    <body bgcolor="#FF0000"> </body> 
+	<div class="navbar">
+		<div class="container">
+			<div class="row">
+				<div class="col s3">
+					<div class="content-left">
+						<a href="#slide-out" data-activates="slide-out" class="sidebar"><i class="fas fa-bars"></i></a>
+					</div>
+				</div>
+				<div class="col s6">
+					<div class="content-center">
+						<a href="index.html"><h1>Prince AL's Royal</h1></a>
+					</div>
+				</div>
+				<div class="col s3">
+					<div class="content-right">
+						<a href="#slide-out-right" data-activates="slide-out-right" class="sidebar-right"><i class="fas fa-user"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    
+    <div style="text-align:center;">
+        <img src="https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1558286670307&di=0966b4a17f191e8e4ca2505aee5c36a3&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20180829%2F4e38eb983b7446cba6009d2d863517d3.jpeg " width="1050" height="400">
+    </div>
+	<div class="booking segments-page">
+		<div class="container">
+			<div class="wrapper-content">
+				<form>
+					<input type="text" class="datepicker" placeholder="Check In">
+					<input type="text" class="datepicker" placeholder="Check Out">
+					<select class="browser-default select-type">
+					    <option value="" disabled selected>Room Type</option>
+					    <option value="1">Family Room</option>
+					    <option value="2">Superior Room</option>
+					    <option value="3">Deluxe Room</option>
+					</select>
+					<div class="row">
+						<div class="col s4">
+							<select class="browser-default">
+							    <option value="" disabled selected>Adult</option>
+							    <option value="1">1</option>
+							    <option value="2">2</option>
+							    <option value="3">3</option>
+							  </select>
+						</div>
+						<div class="col s4">
+							<select class="browser-default">
+							    <option value="" disabled selected>Children</option>
+							    <option value="1">0</option>
+							    <option value="1">1</option>
+							    <option value="2">2</option>
+							    <option value="3">3</option>
+							  </select>
+						</div>
+						<div class="col s4">
+							<select class="browser-default">
+							    <option value="" disabled selected>Rooms</option>
+							    <option value="1">1</option>
+							    <option value="2">2</option>
+							    <option value="3">3</option>
+							  </select>
+						</div>
+					</div>
+					<button class="button">Submit</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<div class="best-rooms segments">
+		<div class="container">
+			<div class="section-title">
+				<h3>The preview room</h3>
+			</div>
+			<div class="row">
+				<div class="col s6">
+					<div class="content-left">
+						<img src="images/best-room1.jpg" alt="">
+					</div>
+				</div>
+				<div class="col s6">
+					<div class="content-right">
+						<a href="room-details.html"><h5>Superior Room</h5></a>
+						<h6>$50<span>/Night</span></h6>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s6">
+					<div class="content-left">
+						<img src="images/best-room2.jpg" alt="">
+					</div>
+				</div>
+				<div class="col s6">
+					<div class="content-right">
+						<a href="room-details.html"><h5>Family Room</h5></a>
+						<h6>$50<span>/Night</span></h6>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col s6">
+					<div class="content-left">
+						<img src="images/best-room3.jpg" alt="">
+					</div>
+				</div>
+				<div class="col s6">
+					<div class="content-right">
+						<a href="room-details.html"><h5>Delux Room</h5></a>
+						<h6>$50<span>/Night</span></h6>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+</body>
+</html>
